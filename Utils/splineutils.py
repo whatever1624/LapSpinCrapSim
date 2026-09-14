50e36d5c21bd6f55ccac168f002d22a31f11e7e9
Willow <chanwl0204@gmail.com>
2026-09-14 16:13:48 +0100

    splinend param method

--- Utils/splineutils.py
+++ Utils/splineutils.py
@@ -9,6 +9,7 @@ import scipy
 import numpy as np
 
 # Project python modules
+import Utils.utils as utils
 from Utils.typealiases import Any, NDArrayFloat1D, NDArrayFloat2D, NDArrayNumber1D
 
 
@@ -18,8 +19,8 @@ class SplineND:
 
     The spline is a scipy.interpolate.BSpline with degree 3 (k=3), created using scipy.interpolate.make_splprep()
 
-    Note: A wrapper class is preferred for performance reasons as it allows pre-computing the (somewhat expensive)
-    derivative and antiderivative splines, which are also used within the spline helper methods
+    Note: A wrapper class is preferred for performance reasons as it allows pre-computing spline properties - derivative
+    spline and displacement LUT
 
     Methods:
         ...
@@ -43,8 +44,12 @@ class SplineND:
         # Dimension of the spline
         self.NDim = data.shape[1]
 
-        # Calculate u parameterization if not provided, as the approximate displacement along the spline
-        # Uses the linear distance between each data point corrected by approximated curvature between each data point
+        # Calculate u parameterization as the (approximate) displacement along the spline, if not provided
+        # Calculated using circular segment length - refer to: https://en.wikipedia.org/wiki/Circular_segment#Formulae
+        #   Arc length = Sector angle / Curvature
+        #   Chord length = 2 * sin(Sector angle / 2) / Curvature
+        #   Rearranging: Curvature = 2 * sin(Sector angle / 2) / Chord length
+        #   Therefore: Arc length = Sector angle / 2 / sin(Sector angle / 2) * Chord length
         if u is None:
             dataDiff = np.diff(data, axis=0)
             dot = (np.linalg.vecdot(dataDiff[:-1], dataDiff[1:])
@@ -54,7 +59,6 @@ class SplineND:
                 ADiffBound = np.acos(np.dot(dataDiff[-1], dataDiff[0])
                                      / np.linalg.norm(dataDiff[-1]) / np.linalg.norm(dataDiff[0]))
                 ADiff = (np.hstack([ADiffBound, ADiff]) + np.hstack([ADiff, ADiffBound])) / 2
-
             else:
                 ADiff = (np.hstack([ADiff[0], ADiff]) + np.hstack([ADiff, ADiff[-1]])) / 2
             indsCurv = np.where(ADiff != 0)[0]
@@ -65,17 +69,14 @@ class SplineND:
         # Create spline to interpolate/fit the data
         self.spline, self.uData = scipy.interpolate.make_splprep(data.T, u=u, k=3, s=smoothing,
                                                                  bc_type='periodic' if BCyclic else 'not-a-knot')
-        self.uMin = min(self.uData)
-        self.uMax = max(self.uData)
+        self.uMin: float = min(self.uData)
+        self.uMax: float = max(self.uData)
 
         # Pre-compute spline derivatives
         self.splineDer = self.spline.derivative(nu=1)
         self.splineDer2 = self.spline.derivative(nu=2)
 
-        # Pre-compute spline antiderivative - TODO: AntiDer isn't used i think?
-        self.splineAntiDer = self.spline.antiderivative(nu=1)
-
-        # Pre-compute displacement LUT along the spline - TODO: Consider making this a 1D spline?
+        # Pre-compute displacement LUT along the spline - LUT is preferred over spline for speed and size reasons
         self.uLUT = np.linspace(self.uMin, self.uMax, 1 + int(np.ceil((self.uMax - self.uMin) / uResolutionLUT)))
         self.sLUT = np.append(0, scipy.integrate.cumulative_trapezoid(
                               np.linalg.norm(self.calc_tangent(self.uLUT, BNormalise=False), axis=-1), self.uLUT))
@@ -111,7 +112,7 @@ class SplineND:
         return self.splineDer(u).T
 
     def evaluate_der2(self,
-                     u: float | list[float] | NDArrayFloat1D) -> NDArrayFloat1D | NDArrayFloat2D:
+                      u: float | list[float] | NDArrayFloat1D) -> NDArrayFloat1D | NDArrayFloat2D:
         """
         Evaluate the spline second derivative at parameter value(s) u
 
@@ -124,21 +125,6 @@ class SplineND:
             For multiple parameter values, the derivatives will be vertically stacked
         """
         return self.splineDer2(u).T
-
-    def evaluate_antider(self,
-                         u: float | list[float] | NDArrayFloat1D) -> NDArrayFloat1D | NDArrayFloat2D:
-        """
-        Evaluate the spline antiderivative at parameter value(s) u
-
-        Args:
-            u: Parameter value(s)
-
-        Returns:
-            Antiderivative of the spline point(s) with respect to the spline parameter u at the u parameter value(s)
-
-            For multiple parameter values, the antiderivatives will be vertically stacked
-        """
-        return self.splineAntiDer(u).T
 
     def calc_tangent(self,
                      u: float | list[float] | NDArrayFloat1D,
@@ -160,6 +146,28 @@ class SplineND:
         """
         tangent = self.evaluate_der(u)
         return (tangent.T / np.linalg.norm(tangent, axis=-1)).T if BNormalise else tangent
+
+    def calc_tangentDer(self,
+                        u: float | list[float] | NDArrayFloat1D,
+                        BUnit: bool = True) -> NDArrayFloat1D | NDArrayFloat2D:
+        """
+        Calculate the derivative of the tangent vector at parameter value(s) u
+
+        Args:
+            u: Parameter value(s)
+            BUnit: Whether to use a unit tangent vector for the derivative
+
+        Returns:
+            Derivative of the tangent vector(s) at the u parameter values(s) along the spline
+
+            The derivative of the tangent vector(s) will be for the unit tangent vector(s) is BUnit, otherwise the
+            length of the returned vector will be equal to the acceleration of the spline
+
+            For multiple parameter values, the derivatives of the tangent vectors will be vertically stacked
+        """
+        tangentDer = self.evaluate_der2(u)
+        return (tangentDer.T / np.linalg.norm(self.calc_tangent(u, BNormalise=False), axis=-1)).T \
+               if BUnit else tangentDer
 
     def calc_normal_lat(self,
                         u: float | list[float] | NDArrayFloat1D,
@@ -221,11 +229,12 @@ class SplineND:
         tangentT = self.calc_tangent(u, BNormalise=False).T
         return np.arctan2(tangentT[1], tangentT[0])
 
-
     def calc_kVec(self,
                   u: float | list[float] | NDArrayFloat1D) -> NDArrayFloat1D | NDArrayFloat2D:
         """
         Calculate the curvature vector at parameter value(s) u
+
+        Definition of curvature reference: https://en.wikipedia.org/wiki/Curvature#General_parametrization
 
         Args:
             u: Parameter value(s)
@@ -235,9 +244,8 @@ class SplineND:
 
             For multiple parameter values, the curvature vectors will be vertically stacked
         """
-        return (self.evaluate_der2(u).T /
-                np.pow(np.linalg.norm(self.calc_tangent(u, BNormalise=False).T, axis=0), 2)).T
-
+        return (self.calc_tangentDer(u, BUnit=False).T
+                / np.pow(np.linalg.norm(self.calc_tangent(u, BNormalise=False).T, axis=0), 2)).T
 
     def calc_kTotal(self,
                     u: float | list[float] | NDArrayFloat1D) -> float | NDArrayFloat1D:
@@ -288,6 +296,9 @@ class SplineND:
         """
         Calculate displacement along the spline from uStart to uEnd, by linearly interpolating the pre-computed LUT
 
+        Note that there is no internal validation that uMin <= u <= uMax, so the displacement returned will be as if u
+        is clamped to be between uMin and uMax
+
         Args:
             u: Parameter value(s)
 
@@ -296,12 +307,97 @@ class SplineND:
         """
         return np.interp(u, self.uLUT, self.sLUT)
 
-    def param_closest(self,
-                      data: NDArrayFloat2D,
-                      uSeed: float | None = None) -> NDArrayFloat1D:
-        ...
+    def param(self,
+              data: NDArrayFloat2D,
+              uSeed: float | None = None,
+              BFast: bool = False) -> NDArrayFloat1D:
+        """
+        Parameterise data points by closest point on the spline
 
-    def param_normal(self,
-                     data: NDArrayFloat2D,
-                     uSeed: float | None = None) -> NDArrayFloat1D:
-        ...
+        By definition, this will parameterise the data points to be normal to the spline tangent
+
+        This is a local optimisation starting at uSeed for the first data point, and subsequently using the
+        parameterization of the previous point as the seed for the next data point
+
+        Args:
+            data: TODO
+            uSeed: Initial u parameter candidate for the first data point, uses self.uMin if not provided
+            BFast: Whether to skip the additional root-finding refinement of the parameterisation
+
+        Returns:
+            Parameterization of data points for closest point to the spline
+
+        Raises:
+            ValueError: Failed to converge for point {(i + 1)}/{len(data)} ({res.message})
+        """
+        if uSeed is None:
+            uSeed = self.uMin
+
+        def funjac_closest(u: NDArrayFloat1D,
+                           point: NDArrayFloat1D) -> tuple[float, float]:
+            """
+            Calculate the objective function and its Jacobian for the first pass minimisation algorithm
+
+            This is the distance of the spline at parameter u from the data point, and its derivative with respect to u
+
+            Args:
+                u: Candidate u parameter
+                point: Data point
+
+            Returns:
+                Tuple of (obj, jac)
+
+                obj: Objective function (distance of the spline at parameter u from the data point)
+
+                jac: Jacobian of the objective function
+            """
+            u = utils.wrap(u[0], self.uMin, self.uMax)
+            vec = self.evaluate(u) - point
+            lVec = np.linalg.norm(vec)
+            return lVec, np.dot(self.calc_tangent(u, BNormalise=False), vec / lVec)
+
+        def fun_normal(u: NDArrayFloat1D,
+                       point: NDArrayFloat1D) -> float:
+            """
+            Calculate the objective function for the second pass root-finding algorithm
+
+            This is the cosine of the angle between the spline tangent and the vector from the spline at parameter u to
+            the data point
+
+            Args:
+                u: Candidate u parameter
+                point: Data point
+
+            Returns:
+                Objective function (cosine of the angle between the spline tangent and the vector from the spline at
+                parameter u to the data point)
+            """
+            u = utils.wrap(u, self.uMin, self.uMax)
+            vec = self.evaluate(u) - point
+            return np.dot(self.calc_tangent(u), vec / np.linalg.norm(vec))
+
+        # First pass - closest point minimisation
+        method = 'BFGS'  # Slightly faster than 'Newton-CG', and much faster than other non-hessian methods
+        fallbackMethod = 'Newton-CG' if method == 'BFGS' else 'BFGS'
+        uParam = np.zeros(len(data))
+        for i, point in enumerate(data):
+            res = scipy.optimize.minimize(funjac_closest, uSeed, args=point, method=method, jac=True)
+            if not res.success:
+                # Fallback solver method
+                res = scipy.optimize.minimize(funjac_closest, uSeed, args=point, method=fallbackMethod, jac=True)
+                if not res.success:
+                    raise ValueError(f"Failed to converge for point {(i + 1)}/{len(data)} ({res.message})")
+            uSeed = res.x[0]
+            uParam[i] = res.x[0]
+
+        # Second pass - dot product root-finding
+        if not BFast:
+            method = 'secant'  # Around 2x faster than newton (no derivative provided)
+            for i, point in enumerate(data):
+                # TODO: provide the derivative (idk if it'll work bc every other time i've tried calculating has failed)
+                sol = scipy.optimize.root_scalar(fun_normal, args=point, method=method, x0=uParam[i])
+                if not sol.converged:
+                    raise ValueError(f"Failed to converge for point {(i + 1)}/{len(data)} ({sol.flag})")
+                uParam[i] = sol.root
+
+        return uParam
